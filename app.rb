fba27535fe68537620ca9cb337670caf6df2631a
Marko Klemetti <marko.klemetti@gmail.com>
2013-10-29 16:06:59 +0200

    Add build types and statuses

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require 'teamcity'
 set :haml, format: :html5
  
 get "/" do
-  @projects = TeamCity.projects
+  @projects = TeamCity.projects.reject{|k| k.id == "_Root"}.select{|k| (k.name == "Apache Ivy")}
+  @jobs = @states = {}
+
+  @projects.each do |p|
+    @jobs[p.id] = TeamCity.project_buildtypes(id: p.id)
+    TeamCity.project_buildtypes(id: p.id).each { |bt|
+      @states[bt.id] = TeamCity.builds(buildType: bt.id).last.status == "SUCCESS" ? "green" : "red"
+    }
+  end
+
   haml :index
 end
