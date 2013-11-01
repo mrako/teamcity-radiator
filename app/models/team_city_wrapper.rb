b563f8c22dfb79f0f2b71cb19504039ee78678cc
Marko Klemetti <marko.klemetti@gmail.com>
2013-11-01 10:32:18 +0200

    Add build date

--- app/models/team_city_wrapper.rb
+++ app/models/team_city_wrapper.rb
@@ -12,18 +12,27 @@ class TeamCityWrapper
   def self.collect_buildtypes(id)
     Array.new.tap do |types|
       TeamCity.project_buildtypes(id: id).each { |b|
-        types << {name: b.name, href: b.webUrl, status: build_status_for_type(b.id)}
+        types << {name: b.name, href: b.webUrl}.merge(build_info_for_type(b.id))
       }
     end
   end
 
-  def self.build_status_for_type(id)
+  def self.build_info_for_type(id)
     builds = TeamCity.builds(buildType: id)
+    Hash.new.tap { |build|
+      build[:status] = (builds.empty? ? "empty" : builds.first.status.downcase)
+      build[:start] = build_date(builds)
+    }
+  end
 
-    builds.empty? ? "empty" : builds.last.status.downcase
+  def self.build_date(builds)
+    return if builds.empty?
+    time = DateTime.parse(builds.first.startDate)
+
+    time.strftime("%d.%m.%Y %H:%M") if time
   end
 
   def self.projects
-    TeamCity.projects.reject{|k| k.id == "_Root"}.select{|k| (k[:name] == "Apache Ivy")}
+    TeamCity.projects.reject{|k| k.id == "_Root"}
   end
 end
--- spec/models/team_city_wrapper_spec.rb
+++ spec/models/team_city_wrapper_spec.rb
@@ -10,13 +10,14 @@ describe TeamCityWrapper do
 
       TeamCity.should_receive(:projects).and_return(projects)
 
-      TeamCityWrapper.should_receive(:collect_buildtypes).with(1).and_return([{name: "Kivikunu", status: "green"}])
+      TeamCityWrapper.should_receive(:collect_buildtypes).with(1).and_return([{name: "Kivikunu", status: "success", start: "21.10.2013 10:00"}])
 
       projects = TeamCityWrapper.collect_projects.first
 
       projects[:name].should == "Tomme"
       projects[:types].first[:name].should == "Kivikunu"
-      projects[:types].first[:status].should == "green"
+      projects[:types].first[:status].should == "success"
+      projects[:types].first[:start].should == "21.10.2013 10:00"
     end
   end
 
@@ -25,30 +26,31 @@ describe TeamCityWrapper do
       build_types = [OpenStruct.new({name: "build_111"}), OpenStruct.new({name: "build_222"})]
       TeamCity.should_receive(:project_buildtypes).with(id: "bt11").and_return(build_types)
 
-      TeamCityWrapper.should_receive(:build_status_for_type).twice.and_return("green")
+      TeamCityWrapper.should_receive(:build_info_for_type).twice.and_return({status: "success", start: "21.10.2013 10:00"})
 
       types = TeamCityWrapper.collect_buildtypes("bt11").first
 
       types[:name].should == "build_111"
-      types[:status].should == "green"
+      types[:status].should == "success"
+      types[:start].should == "21.10.2013 10:00"
     end
   end
 
-  describe "build_status_for_type" do
-    it "should return last succeeding build status" do
-      builds = [OpenStruct.new({status: "FAILURE"}), OpenStruct.new({status: "SUCCESS"})]
+  describe "build_info_for_type" do
+    it "should return last succeeding build info" do
+      builds = [OpenStruct.new({status: "FAILURE", startDate: "20131002T091700+0400"}), OpenStruct.new({status: "SUCCESS", startDate: "20131006T091800+0400"})]
 
       TeamCity.should_receive(:builds).with(buildType: "build_123").and_return(builds)
 
-      TeamCityWrapper.build_status_for_type("build_123").should == "success"
+      TeamCityWrapper.build_info_for_type("build_123").should == {:status=>"failure", :start=>"02.10.2013 09:17"}
     end
 
     it "should return last failing build status" do
-      builds = [OpenStruct.new({status: "FAILURE"})]
+      builds = [OpenStruct.new({status: "SUCCESS", startDate: "20131006T091800+0400"})]
 
       TeamCity.should_receive(:builds).with(buildType: "build_111").and_return(builds)
 
-      TeamCityWrapper.build_status_for_type("build_111").should == "failure"
+      TeamCityWrapper.build_info_for_type("build_111").should == {:status=>"success", :start=>"06.10.2013 09:18"}
     end
   end
 
